966a113f65a76d79c7453e80af4ff1cb1809b6b8
Jingen Lin <jingen.lin.jl@gmail.com>
2013-09-12 17:57:40 -0400

    fix : 2013-09-12 17:57:40 -0400 Thursday

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130911185921) do
+ActiveRecord::Schema.define(version: 20130912215513) do
 
   create_table "issues", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "no_followers"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "project_id"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
