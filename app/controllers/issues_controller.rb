3077d8e82c87ac7081b32fe7885fd2734ca0cd05
Jingen Lin <jingen.lin.jl@gmail.com>
2013-09-12 17:48:24 -0400

    fix : 2013-09-12 17:48:24 -0400 Thursday

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,85 @@
+class IssuesController < ApplicationController
+  before_action :set_issue, only: [:show, :edit, :update, :destroy]
+  before_action :validate_user, only: [ :index, :show] # "expect" keyword for excluding 
+  #before_action :validate_user, except: [ :index, :show]
+
+  # GET /issues
+  # GET /issues.json
+  layout "issue_layout"
+  def index
+    @issues = Issue.all
+    # @issue = @issues[0]
+    # render "issues/new"
+  end
+
+  # GET /issues/1
+  # GET /issues/1.json
+  def show
+  end
+
+  # GET /issues/new
+  def new
+    @issue = Issue.new
+  end
+
+  # GET /issues/1/edit
+  def edit
+  end
+
+  # POST /issues
+  # POST /issues.json
+  def create
+    @issue = Issue.new(issue_params)
+
+    flash[:notice] = "Issue ..."
+    respond_to do |format|
+      if @issue.save
+        # format.html {redirect_to @issue, notice: 'Issue was successfully created.' }
+        format.html {redirect_to @issue }
+        format.json { render action: 'show', status: :created, location: @issue }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @issue.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /issues/1
+  # PATCH/PUT /issues/1.json
+  def update
+    respond_to do |format|
+      if @issue.update(issue_params)
+        format.html { redirect_to @issue, notice: 'Issue was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @issue.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /issues/1
+  # DELETE /issues/1.json
+  def destroy
+    @issue.destroy
+    respond_to do |format|
+      format.html { redirect_to issues_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    #validate_user
+    def validate_user
+      puts "validate_user"
+    end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_issue
+      @issue = Issue.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def issue_params
+      params.require(:issue).permit(:title, :description, :no_followers)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewIssues::Application.config.secret_key_base = '0b37229a8f8298bca9135db86eb00a5e76d4a701d9af7a1d1ed73b2141a77ce98737577a559a71f18812ec0ef1c01ccaeb609097a13ff24205d58e77aacaf039'
